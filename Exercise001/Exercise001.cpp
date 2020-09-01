65f591eb3095a6d9fb1db591bb1506693fad1710
YleniaChitano <116096@stud.inn.no>
2020-09-01 15:36:36 +0200

    Last version.

--- Exercise001/Exercise001.cpp
+++ Exercise001/Exercise001.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <string>
+
 int age;
+std::string name = "";
 
 int main()
 {
     std::cout << "Please enter your age ";
     std::cin >> age;
+    std::cout << "What is your full name? ";
+    std::cin.ignore();
+    std::getline(std::cin, name);
+
     if (age >= 20) {
-        std::cout << "You are an adult";
+        std::cout << "You are an adult\n";
     }
     else {
-        std::cout << "You are a teenager";
+        std::cout << "You are a teenager\n";
     }
+    std::cout << "Your name is " << name << " and you are " << age << " years old. Good for you!" << std::endl;
+
+    return 0;
 }
 
